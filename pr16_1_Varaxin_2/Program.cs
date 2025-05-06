56f205051ca4d3efd8538a37395787b3bac29cb8
al430 <126333805+al430@users.noreply.github.com>
2025-05-06 14:34:39 +0500

    Add files via upload

--- pr16_1_Varaxin_2/Program.cs
+++ pr16_1_Varaxin_2/Program.cs
@@ -1,53 +1,23 @@
-﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace pr16_3_Varaxin
+namespace pr16_1_Varaxin_2
 {
-    internal class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            try
-            {
-                Console.WriteLine("Введите размерность массива");
-                int n = Convert.ToInt32(Console.ReadLine());
-                double[] startArr = new double[n];
-
-                for (int i = 0; i < n; i++)
-                {
-                    Console.WriteLine($"Введите {i + 1} значение");
-                    startArr[i] = Convert.ToDouble(Console.ReadLine());
-                }
-
-                Dictionary<double, int> dict = new Dictionary<double, int>();
-
-                Console.WriteLine("----------");
-
-                for (int i = 0; i < n; i++)
-                {
-                    if (!dict.ContainsKey(startArr[i]))
-                    {
-                        int count = startArr.Count(p => p == startArr[i]);
-                        Console.WriteLine($"{startArr[i]} - {count}");
-                        dict.Add(startArr[i], count);
-                    }
-                }
-
-                Console.WriteLine("----------");
-
-                foreach (var d in dict)
-                {
-                    Console.WriteLine($"{d.Key * d.Value} - {d.Value}");
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Неверный формат");
-            }
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
